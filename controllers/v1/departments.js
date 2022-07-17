59ba86abc8779352085977ef0d617b80bd5cc932
Steffen Geving <gevis1@student.op.ac.nz>
2022-07-18 09:21:16 +1200

    feat: add routes and controllers

---
+++ controllers/v1/departments.js
@@ -0,0 +1,128 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const getDepartment = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const department = await prisma.department.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!department) {
+      return res
+        .status(200)
+        .json({ msg: `No department with the id: ${id} found` });
+    }
+
+    return res.json({ data: department });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
+
+const getDepartments = async (req, res) => {
+  try {
+    const departments = await prisma.department.findMany();
+
+    if (departments.length === 0) {
+      return res.status(200).json({ msg: "No departments found" });
+    }
+
+    return res.json({ data: departments });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
+
+const createDepartment = async (req, res) => {
+  try {
+    const { name, institutionId } = req.body;
+
+    await prisma.department.create({
+      data: { name, institutionId },
+    });
+
+    const newDepartments = await prisma.department.findMany();
+
+    return res.status(201).json({
+      msg: "Department successfully created",
+      data: newDepartments,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
+
+const updateDepartment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, institutionId } = req.body;
+
+    let department = await prisma.department.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!department) {
+      return res
+        .status(200)
+        .json({ msg: `No department with the id: ${id} found` });
+    }
+
+    department = await prisma.department.update({
+      where: { id: Number(id) },
+      data: { name, institutionId },
+    });
+
+    return res.json({
+      msg: `Department with the id: ${id} successfully updated`,
+      data: department,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
+
+const deleteDepartment = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const department = await prisma.department.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!department) {
+      return res
+        .status(200)
+        .json({ msg: `No department with the id: ${id} found` });
+    }
+
+    await prisma.department.delete({
+      where: { id: Number(id) },
+    });
+
+    return res.json({
+      msg: `Department with the id: ${id} successfully deleted`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
+
+export {
+  getDepartment,
+  getDepartments,
+  createDepartment,
+  updateDepartment,
+  deleteDepartment,
+};
---
+++ routes/v1/departments.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+const router = Router();
+
+import {
+  getDepartment,
+  getDepartments,
+  createDepartment,
+  updateDepartment,
+  deleteDepartment,
+} from "../../controllers/v1/departments.js";
+
+router.route("/").get(getDepartments).post(createDepartment);
+router
+  .route("/:id")
+  .get(getDepartment)
+  .put(updateDepartment)
+  .delete(deleteDepartment);
+
+export default router;
