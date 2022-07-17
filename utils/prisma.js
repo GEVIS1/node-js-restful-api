5c6a97116a6ffda1552cfcb90a62bafdc1e456f9
Steffen Geving <gevis1@student.op.ac.nz>
2022-07-18 11:26:41 +1200

    feat: add Department and Institution exports

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,3 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+const { Department, Institution } = prisma
 export default prisma
+export { Department, Institution }
