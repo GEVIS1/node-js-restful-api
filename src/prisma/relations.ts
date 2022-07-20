dadcaed34e516de8b2306f34c7299b93752b5c38
Steffen Geving <gevis1@student.op.ac.nz>
2022-07-20 15:25:33 +1200

    feat: add Relation type

--- src/prisma/relations.ts
+++ src/prisma/relations.ts
@@ -1,6 +1,9 @@
 /**
  * I have not found a programmatic way of extracting the relation fields from prisma yet, so for now store them as strings in here
  */
-const institutionRelations = {}
-const departmentRelations = { include: { institution: true } }
-export { institutionRelations, departmentRelations }
+
+export type Relation = {
+    include: {}
+}
+export const institutionRelations: Relation = { include: { }}
+export const departmentRelations: Relation = { include: { institution: true } }
