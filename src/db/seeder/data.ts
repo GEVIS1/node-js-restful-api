145e4031991626a7883f9ed633e4d30936a3d5d8
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-07-26 22:16:23 +1200

    feat: add seed data url strings in db/seeder/data.ts

---
+++ src/db/seeder/data.ts
@@ -0,0 +1,4 @@
+export const departmentsURL =
+  'https://gist.githubusercontent.com/GEVIS1/8d027c13e904f9821e4809aab29c7430/raw/6037ee98e00383e7633d7b9adecd88d5c21fd224/departments.json';
+export const institutionsURL =
+  'https://gist.githubusercontent.com/GEVIS1/8d027c13e904f9821e4809aab29c7430/raw/6037ee98e00383e7633d7b9adecd88d5c21fd224/institutions.json';
