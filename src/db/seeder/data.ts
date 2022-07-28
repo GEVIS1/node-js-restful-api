96218ed190196cb19c3266e823372d424a195789
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-07-28 23:30:04 +1200

    feat(seeder:data.ts): add institutionsarray

--- src/db/seeder/data.ts
+++ src/db/seeder/data.ts
@@ -1,4 +1,5 @@
-export const departmentsURL =
-  'https://gist.githubusercontent.com/GEVIS1/8d027c13e904f9821e4809aab29c7430/raw/2045b0a91215a95e036166f36d8b997bd22706a2/departments.json';
-export const institutionsURL =
-  'https://gist.githubusercontent.com/GEVIS1/8d027c13e904f9821e4809aab29c7430/raw/2045b0a91215a95e036166f36d8b997bd22706a2/institutions.json';
+const gistURL =
+  'https://gist.githubusercontent.com/GEVIS1/8d027c13e904f9821e4809aab29c7430/raw/66606095ec88a3be46dc9a390451f1d15f7372d0/';
+export const departmentsURL = `${gistURL}departments.json`;
+// export const institutionsURL = `${gistURL}institutions.json`;
+export const institutionsURL = `${gistURL}institutionsarray.json`;
