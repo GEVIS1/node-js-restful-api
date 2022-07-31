1d221db81729515c210a119d2bd0af544a15f31e
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-07-31 23:36:42 +1200

    feat(routes/index): add auth to routes index

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,4 +1,5 @@
 import departments from './departments';
 import institutions from './institutions';
+import auth from './auth';
 
-export { departments, institutions };
+export { departments, institutions, auth };
