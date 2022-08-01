0f1eb041c26954c38eaceac6908938910292351d
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-02 11:33:52 +1200

    refactor(checkAuthorization.ts): return true or false instead of end http transaction

--- src/middleware/authorization/checkAuthorization.ts
+++ src/middleware/authorization/checkAuthorization.ts
@@ -1,16 +1,19 @@
 import PrismaClient from '../../utils/prisma/prisma';
-import { StatusCodes } from 'http-status-codes';
-import { Response } from 'express';
 
-const checkAuthorization = async (id: string, res: Response) => {
+const AuthorizedRoles = ['ADMIN_USER', 'SUPER_ADMIN_USER'] as const;
+type AuthorizedRole = typeof AuthorizedRoles[number];
+
+/**
+ * Function that checks if user is of role "ADMIN_USER", or "SUPER_ADMIN_USER".
+ * @param id The id number of the user to check
+ * @returns true if authorized, false if not
+ */
+const checkAuthorization = async (id: string): Promise<boolean> => {
   const user = await PrismaClient.user.findUnique({
     where: { id: Number(id) },
   });
-  if (user.role !== 'ADMIN_USER') {
-    return res.status(StatusCodes.FORBIDDEN).json({
-      msg: 'Not authorized to access this route',
-    });
-  }
+  const { role } = user;
+  return AuthorizedRoles.includes(role as AuthorizedRole);
 };
 
 export default checkAuthorization;
