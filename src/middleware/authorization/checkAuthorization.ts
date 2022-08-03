7eef4e8d7236e936f35fad285682a972d6763095
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-03 22:14:09 +1200

    fix(checkAuthorization.ts): replace undefined check with null
    upon futher investigation fundFirst returns null if nothing is found, not undefined

--- src/middleware/authorization/checkAuthorization.ts
+++ src/middleware/authorization/checkAuthorization.ts
@@ -1,19 +1,52 @@
+import { Role } from '@prisma/client';
 import PrismaClient from '../../utils/prisma/prisma';
 
-const AuthorizedRoles = ['ADMIN_USER', 'SUPER_ADMIN_USER'] as const;
-type AuthorizedRole = typeof AuthorizedRoles[number];
+const AuthorizedRoles: Role[] = ['ADMIN_USER', 'SUPER_ADMIN_USER'];
+const SuperAuthorizedRole = 'SUPER_ADMIN_USER';
 
 /**
- * Function that checks if user is of role "ADMIN_USER", or "SUPER_ADMIN_USER".
- * @param id The id number of the user to check
+ * Function that creates a function checking if user is of one of the roles in permissions.
+ * @param permissions String (array) of the allowed roles
  * @returns true if authorized, false if not
  */
-const checkAuthorization = async (id: string): Promise<boolean> => {
-  const user = await PrismaClient.user.findUnique({
-    where: { id: Number(id) },
-  });
-  const { role } = user;
-  return AuthorizedRoles.includes(role as AuthorizedRole);
-};
+const authorizationBase =
+  async (permissions: Role | Role[]) =>
+  async (id: string): Promise<boolean> => {
+    /**
+     * Query the database for the user id where their role is in the permissions
+     * This will return the user object if user has permissions and undefined if
+     * there is no user with that id and those permissions.
+     */
+    const user = await PrismaClient.user.findFirst({
+      where: {
+        id: {
+          equals: Number(id),
+        },
+        role: {
+          in: permissions,
+        },
+      },
+    });
 
-export default checkAuthorization;
+    /**
+     * Return true if user has permissions (user was found),
+     * false if not (in which case user was not found and is null).
+     */
+    return user !== null;
+  };
+
+/**
+ * Function that checks if a user is of the authorization "ADMIN_USER" or "SUPER_ADMIN_USER"
+ * @param id The id number of the user to check
+ * @returns a promise to a boolean representing if the user is authorized
+ */
+const checkAuthorization = await authorizationBase(AuthorizedRoles);
+
+/**
+ * Function that checks if a user is of the authorization "SUPER_ADMIN_USER"
+ * @param id The id number of the user to check
+ * @returns a promise to a boolean representing if the user is authorized
+ */
+const checkSuperAuthorization = await authorizationBase(SuperAuthorizedRole);
+
+export { checkAuthorization, checkSuperAuthorization };
