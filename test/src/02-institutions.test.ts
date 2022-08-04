97978061e1def9ea7ef602e57aebb4ab4f2be190
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-04 20:44:05 +1200

    refactor/style: syntax fixes

--- test/src/02-institutions.test.ts
+++ test/src/02-institutions.test.ts
@@ -14,13 +14,13 @@ describe('It should manipulate institutions', async () => {
     agent
       .post('/api/v1/auth/login')
       .send({ username, password })
-      .then((authRes) => {
-        const { token } = authRes.body.data;
+      .end((_, authRes) => {
+        const { token } = authRes.body;
         agent
           .post('/api/v1/institutions')
           .auth(token, { type: 'bearer' })
           .send(institutions[0])
-          .end((_, res) => {
+          .end((__, res) => {
             chai.expect(res.status).to.be.equal(403);
             chai.expect(res.body).to.be.an('object');
             chai
@@ -39,14 +39,14 @@ describe('It should manipulate institutions', async () => {
     agent
       .post('/api/v1/auth/login')
       .send({ username, password })
-      .then((authRes) => {
-        const { token } = authRes.body.data;
+      .end((_, authRes) => {
+        const { token } = authRes.body;
         agent
           .post('/api/v1/institutions')
           .auth(token, { type: 'bearer' })
           .send(institutions[0])
-          .end((_, res) => {
-            chai.expect(res.status).to.be.equal(403);
+          .end((__, res) => {
+            chai.expect(res.status).to.be.equal(201);
             chai.expect(res.body).to.be.an('object');
             chai
               .expect(res.body.msg)
@@ -65,13 +65,13 @@ describe('It should manipulate institutions', async () => {
     agent
       .post('/api/v1/auth/login')
       .send({ username, password })
-      .then((authRes) => {
-        const { token } = authRes.body.data;
+      .end((_, authRes) => {
+        const { token } = authRes.body;
         agent
           .delete('/api/v1/institutions/1')
           .auth(token, { type: 'bearer' })
           .send()
-          .end((_, res) => {
+          .end((__, res) => {
             chai.expect(res.status).to.be.equal(403);
             chai.expect(res.body).to.be.an('object');
             chai
