d03d48117239471a194d6d2e5f83ca3a82313b42
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-04 21:00:22 +1200

    test(02-institutions): fix assert on delete instition as super admin

--- test/src/02-institutions.test.ts
+++ test/src/02-institutions.test.ts
@@ -1,7 +1,7 @@
 import chai from 'chai';
 
 import { user, adminUser, superAdminUser } from './../misc/userdata';
-import institutions from './../misc/institutiondata';
+import institutions from '../misc/institutiondata';
 //import departments from './../misc/departmentdata';
 import { agent, closeAgent } from './00-setup.test';
 
@@ -87,21 +87,22 @@ describe('It should manipulate institutions', () => {
      * Log back in as the user to fetch the token
      */
     const { username, password } = superAdminUser;
+
     agent
       .post('/api/v1/auth/login')
       .send({ username, password })
-      .then((authRes) => {
-        const { token } = authRes.body.data;
+      .end((_, authRes) => {
+        const { token } = authRes.body;
         agent
           .delete('/api/v1/institutions/1')
           .auth(token, { type: 'bearer' })
           .send()
-          .end((_, res) => {
-            chai.expect(res.status).to.be.equal(403);
+          .end((__, res) => {
+            chai.expect(res.status).to.be.equal(200);
             chai.expect(res.body).to.be.an('object');
             chai
               .expect(res.body.msg)
-              .to.be.equal('Not authorized to access this route');
+              .to.equal('Institution with the id: 1 successfully deleted');
             done();
           });
       });
