19f6de29ec4af0db99b259b38ef69cb0b3b7d72e
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-04 20:48:15 +1200

    fix(util/modelTypes): add id as a wanted property

--- src/utils/prisma/modelTypes.ts
+++ src/utils/prisma/modelTypes.ts
@@ -1,11 +1,13 @@
 import { Prisma } from '@prisma/client';
 
 export const departmentType: Prisma.DepartmentUncheckedCreateInput = {
+  id: 0,
   name: 'NULL',
   institutionId: Infinity,
 };
 
 export const institutionType: Prisma.InstitutionUncheckedCreateInput = {
+  id: 0,
   name: 'NULL',
   region: 'NULL',
   country: 'NULL',
