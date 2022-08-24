8292ca96c136b893f80069ad625cf16234597cf4
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-24 23:10:08 +1200

    refactor: change UserSchema to a curried function
    zod has no functionallity to refer to an adjacent value in the input object to compare two properties, so comparing password and compare fields is not trivial without creating a regex on the fly, same goes for checking that the email address starts with the username. This function creates regex when called with the input object so that this is possible. Also added custom error messages where necessary.

--- src/validators/v2/user.ts
+++ src/validators/v2/user.ts
@@ -1,21 +1,39 @@
+import { Prisma } from '@prisma/client';
 import { z } from 'zod';
 
-const UserSchema = z.object({
-  firstname: z.string().min(2).max(50),
-  lastname: z.string().min(2).max(50),
-  username: z.string().min(5).max(10),
-  email: z.string().email(),
-  password: z
-    .string()
-    .min(8)
-    .max(16)
-    .regex(/.*([a-z][0-9]|[0-9][a-z]).*/),
-  confirm: z
-    .string()
-    .min(8)
-    .max(16)
-    .regex(/.*([a-z][0-9]|[0-9][a-z]).*/),
-  avatar: z.string().url(),
-});
+// Input data with confirm, which doesn't exist in the prisma schema
+type UserCreateInput = Prisma.UserUncheckedCreateInput & { confirm: string };
 
-export { UserSchema };
+const createUserSchema = (data: UserCreateInput) => {
+  return z.object({
+    firstname: z.string().min(2).max(50),
+    lastname: z.string().min(2).max(50),
+    username: z.string().min(5).max(10),
+    email: z
+      .string()
+      .email()
+      .startsWith(data.username, {
+        message: `The Local-part of the email address must match username. I.E: ${
+          data.username
+        }@${data.email.split('@')[1]}`,
+      }),
+    password: z
+      .string()
+      .min(8)
+      .max(16)
+      .regex(/.*([a-z][0-9]|[0-9][a-z]).*/, {
+        message:
+          'The password must contain at least one letter and at least one number.',
+      }),
+    confirm: z
+      .string()
+      .min(8)
+      .max(16)
+      .regex(new RegExp(`${data.confirm}`), {
+        message: 'Passwords did not match.',
+      }),
+    avatar: z.string().url(),
+  });
+};
+
+export { createUserSchema };
