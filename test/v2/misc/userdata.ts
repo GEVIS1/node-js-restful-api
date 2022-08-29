6644df4eb9ef52c61831827e99c40b0ee7fd8b29
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-29 23:41:33 +1200

    test(userdata.ts): add missing confirm field to userdata

--- test/v2/misc/userdata.ts
+++ test/v2/misc/userdata.ts
@@ -1,31 +1,45 @@
 import { Prisma } from '@prisma/client';
-import { Optional } from 'utility-types';
+import { Optional, Assign } from 'utility-types';
 
-type UserCreateInputNoAvatar = Optional<Prisma.UserCreateInput, 'avatar'>;
+export type UserCreateInputNoAvatarWithConfirm = Assign<
+  Optional<Prisma.UserCreateInput, 'avatar'>,
+  { confirm: string }
+>;
 
-export const user: UserCreateInputNoAvatar = {
+/**
+ * The User type, but with 'password' and 'confirm' optional for deletion
+ */
+export type ReturnedUser = Optional<
+  Prisma.UserCreateInput & { confirm?: string },
+  'password'
+>;
+
+export const user: UserCreateInputNoAvatarWithConfirm = {
   firstname: 'Steffen',
   lastname: 'Geving',
   username: 'FenFen',
   email: 'FenFen@geving.com',
   password: 'SteffensPasswo1',
+  confirm: 'SteffensPasswo1',
 };
 
-export const adminUser: UserCreateInputNoAvatar = {
+export const adminUser: UserCreateInputNoAvatarWithConfirm = {
   firstname: 'Neffets',
   lastname: 'Gniveg',
   username: 'EtsEts',
   email: 'EtsEts@gniveg.com',
   password: 'NeffetsPassword1',
+  confirm: 'NeffetsPassword1',
   role: 'ADMIN_USER',
 };
 
-export const superAdminUser: UserCreateInputNoAvatar = {
+export const superAdminUser: UserCreateInputNoAvatarWithConfirm = {
   firstname: 'Sheev',
   lastname: 'Palpatine',
   username: 'DarthSidious',
   email: 'DarthSidious@the.sith',
   password: 'Order 66',
+  confirm: 'Order 66',
   role: 'SUPER_ADMIN_USER',
 };
 
