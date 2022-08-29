7ddcd0f12d71c5f95aae63f0cc436f3b4a268c35
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-29 22:27:22 +1200

    feat(axios.ts): separate baseURL and export for use separately
    also jsdoc the baseURL and getAvatar exports

--- src/utils/v2/axios.ts
+++ src/utils/v2/axios.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+/**
+ * URL to the avatar service used for User creation
+ */
+const baseURL = 'https://avatars.dicebear.com/api/human/';
+/**
+ * An axios instance with the baseURL set to the avatar service used
+ */
 const getAvatar = axios.create({
-  baseURL: 'https://avatars.dicebear.com/api/human/',
+  baseURL,
 });
 
-export { getAvatar };
+export { getAvatar, baseURL };
