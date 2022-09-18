d28b5942cb647a2511ad031055ee82f941e62700
Steffen Geving <gevis1@student.op.ac.nz>
2022-09-19 09:56:13 +1200

    feat(routes/auth): add login route

--- src/routes/v2/auth.ts
+++ src/routes/v2/auth.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 const router = Router();
 
-import { register /*login*/ } from '../../controllers/v2/auth';
+import { register, login } from '../../controllers/v2/auth';
 
 router.route('/register').post(register);
-// router.route('/login').post(login);
+router.route('/login').post(login);
 
 export default router;
