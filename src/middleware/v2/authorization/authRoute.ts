ab726d75ce51af2429e4c9e50391713619fc04b7
Steffen Geving <gevis1@student.op.ac.nz>
2022-09-21 17:45:25 +1200

    feat(middleware): add authRoute middleware
    this middleware will read the authorization header and attempt to verify the JWT within. if it succeeds it passes the verification data to the protected route, otherwise it returns an unauthorized response

---
+++ src/middleware/v2/authorization/authRoute.ts
@@ -0,0 +1,68 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
+
+export interface JWT {
+  id: number;
+  iat: number;
+  exp: number;
+}
+
+/**
+ * Extend the express Request interface to expect the jwt payload
+ */
+export interface AuthorizedRequest extends Request {
+  user?: JWT;
+}
+
+/**
+ * Authentication middleware expecting a bearer token
+ * @argument req The request from the user agent
+ * @argument res The response to be returned to the user agent
+ * @argument next The middleware next function
+ */
+export default (req: AuthorizedRequest, res: Response, next: NextFunction) => {
+  try {
+    /**
+     * The authorization request header contains the information necessary to authenticate a
+     * user agent against the server's protected endpoints.
+     * In this case the information is a bearer token string formatted as "Bearer <token>".
+     * The user agent is a middle man between you and the server. Examples would be Postman or a web browser.
+     */
+    const authHeader = req.headers.authorization;
+
+    /**
+     * A bearer token will look something like this - Bearer <JWT>. A
+     * response containing a 403 forbidden status code and message
+     * is returned if a bearer token is not provided.
+     * One can argue whether this should be a 403 or a 401 error
+     */
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(StatusCodes.FORBIDDEN).json({
+        msg: 'No token provided',
+      });
+    }
+
+    /**
+     * Get the JWT from the bearer token
+     */
+    const [, token] = authHeader.split(' ');
+
+    /**
+     * Verify the signed JWT is valid. The first argument is the token,
+     * i.e., JWT and the second argument is the secret or public/private key.
+     */
+    const payload = jwt.verify(token, process.env.JWT_SECRET as jwt.Secret);
+
+    /**
+     * Set Request's user property to the authenticated user
+     */
+    req.user = payload as JWT;
+
+    return next();
+  } catch (err) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      msg: 'Not authorized to access this route',
+    });
+  }
+};
