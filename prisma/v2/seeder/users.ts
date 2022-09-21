6bc62a49dd498fac3474103d6759fcceee1a98ec
Steffen Geving <gevis1@student.op.ac.nz>
2022-09-21 17:41:59 +1200

    feat(prisma/seeder): add seeder script for prisma

---
+++ prisma/v2/seeder/users.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    firstname: 'Sheev',
+    lastname: 'Palpatine',
+    username: 'DrthSdious',
+    email: 'DrthSdious@the.sith',
+    password: 'Order 66!',
+    confirm: 'Order 66!',
+    role: 'SUPER_ADMIN_USER',
+  },
+  {
+    firstname: 'Yoda',
+    lastname: 'Jedi',
+    username: 'Master',
+    email: 'Master@the.jedi',
+    password: 'TheS1thSux!',
+    confirm: 'TheS1thSux!',
+    role: 'SUPER_ADMIN_USER',
+  },
+];
