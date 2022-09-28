d68dda043bc79adf6583ac6bea647f69e406eb81
Steffen Geving <gevis1@student.op.ac.nz>
2022-09-29 10:36:06 +1300

    test(02-users.test.ts): add test for admin user updating their own information

--- test/v2/src/02-users.test.ts
+++ test/v2/src/02-users.test.ts
@@ -277,4 +277,32 @@ describe('It should update a user by its id', () => {
     chai.expect(putResponse.body.success).to.be.equal(true);
     chai.expect(putResponse.body.data).to.include(compareUser);
   });
+
+  it('should let an admin user update their own information', async () => {
+    const fetchedAdminUser = await getAdminUser();
+    const { username, email, password } = fetchedAdminUser;
+    const loginUser = { username, password };
+    const compareUser = removePasswords(fetchedAdminUser);
+
+    const prismaUser = await prisma?.user.findFirst({
+      where: {
+        username,
+        email,
+      },
+    });
+
+    chai.expect(prismaUser).to.be.an('object');
+
+    const loginResponse = await agent
+      .post('/api/v2/auth/login')
+      .send(loginUser);
+    const putResponse = await agent
+      .put(`/api/v2/users/${prismaUser?.id}`)
+      .set({ Authorization: `Bearer ${loginResponse.body.token}` });
+
+    chai.expect(putResponse.status).to.be.equal(200);
+    chai.expect(putResponse.body).to.be.an('object');
+    chai.expect(putResponse.body.success).to.be.equal(true);
+    chai.expect(putResponse.body.data).to.include(compareUser);
+  });
 });
