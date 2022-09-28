77dd62824375d0154246817e3caa19b6d0042148
Steffen Geving <gevis1@student.op.ac.nz>
2022-09-28 15:02:00 +1300

    test: add tests for SUPER_ADMIN_USER requests to /user

--- test/v2/src/02-users.test.ts
+++ test/v2/src/02-users.test.ts
@@ -2,6 +2,7 @@ import { User } from '@prisma/client';
 import chai from 'chai';
 
 import { removePasswords, user, getAdminUser } from '../misc/userdata';
+import { yoda } from '../../../prisma/v2/seeder/users';
 import { agent } from './00-setup.test';
 
 describe('It should correctly get all the user data a user is authorized for.', () => {
@@ -83,14 +84,75 @@ describe('It should correctly get all the user data a user is authorized for.', 
   });
 
   it('should give BASIC_USER data to a SUPER_ADMIN_USER', (done) => {
-    done();
+    agent
+      .post('/api/v2/auth/login')
+      .send(yoda)
+      .end((_, resLogin) => {
+        agent
+          .get('/api/v2/users')
+          .set({ Authorization: `Bearer ${resLogin.body.token}` })
+          .end((__, res) => {
+            const basicUserData = res.body.data.filter(
+              (u: User) => u.role === 'BASIC_USER'
+            );
+            chai.expect(res.status).to.be.equal(200);
+            chai.expect(res.body).to.be.an('object');
+            chai.expect(res.body.success).to.be.equal(true);
+            chai.expect(basicUserData[0]).to.include(removePasswords(user));
+            done();
+          });
+      });
   });
 
   it('should give ADMIN_USER data to a SUPER_ADMIN_USER', (done) => {
-    done();
+    agent
+      .post('/api/v2/auth/login')
+      .send(yoda)
+      .end((_, resLogin) => {
+        agent
+          .get('/api/v2/users')
+          .set({ Authorization: `Bearer ${resLogin.body.token}` })
+          .end((__, res) => {
+            const adminUserData = res.body.data.filter(
+              (u: User) => u.role === 'ADMIN_USER'
+            );
+            chai.expect(res.status).to.be.equal(200);
+            chai.expect(res.body).to.be.an('object');
+            chai.expect(res.body.success).to.be.equal(true);
+            chai.expect(adminUserData).to.be.an('array');
+            chai.expect(adminUserData.length).to.be.greaterThan(0);
+            adminUserData.forEach((_adminUser: User) => {
+              chai.expect(_adminUser).to.have.property('role');
+              chai.expect(_adminUser.role).to.equal('ADMIN_USER');
+            });
+            done();
+          });
+      });
   });
 
   it('should give SUPER_ADMIN_USER data to a SUPER_ADMIN_USER', (done) => {
-    done();
+    agent
+      .post('/api/v2/auth/login')
+      .send(yoda)
+      .end((_, resLogin) => {
+        agent
+          .get('/api/v2/users')
+          .set({ Authorization: `Bearer ${resLogin.body.token}` })
+          .end((__, res) => {
+            const superAdminUserData = res.body.data.filter(
+              (u: User) => u.role === 'SUPER_ADMIN_USER'
+            );
+            chai.expect(res.status).to.be.equal(200);
+            chai.expect(res.body).to.be.an('object');
+            chai.expect(res.body.success).to.be.equal(true);
+            chai.expect(superAdminUserData).to.be.an('array');
+            chai.expect(superAdminUserData.length).to.be.greaterThan(0);
+            superAdminUserData.forEach((_adminUser: User) => {
+              chai.expect(_adminUser).to.have.property('role');
+              chai.expect(_adminUser.role).to.equal('SUPER_ADMIN_USER');
+            });
+            done();
+          });
+      });
   });
 });
