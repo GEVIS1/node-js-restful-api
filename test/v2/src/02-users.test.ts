5f5bbf3a73cf0badfb6d782e2dfe397b708bcb45
Steffen Geving <gevis1@student.op.ac.nz>
2022-09-28 14:48:04 +1300

    test(02-users.test.ts): add test for getting ADMIN_USER data as ADMIN_USER

--- test/v2/src/02-users.test.ts
+++ test/v2/src/02-users.test.ts
@@ -50,7 +50,36 @@ describe('It should correctly get all the user data a user is authorized for.', 
   });
 
   it('should give ADMIN_USER data to an ADMIN_USER', (done) => {
-    done();
+    getAdminUser().then((adminUser) => {
+      agent
+        .post('/api/v2/auth/login')
+        .send(adminUser)
+        .end((_, resLogin) => {
+          agent
+            .get('/api/v2/users')
+            .set({ Authorization: `Bearer ${resLogin.body.token}` })
+            .end((__, res) => {
+              const adminUserData = res.body.data.filter(
+                (u: User) => u.role === 'ADMIN_USER'
+              );
+              const superAdminUserData = res.body.data.filter(
+                (u: User) => u.role === 'SUPER_ADMIN_USER'
+              );
+              chai.expect(res.status).to.be.equal(200);
+              chai.expect(res.body).to.be.an('object');
+              chai.expect(res.body.success).to.be.equal(true);
+              chai.expect(adminUserData).to.be.an('array');
+              chai.expect(adminUserData.length).to.be.greaterThan(0);
+              adminUserData.forEach((_adminUser: User) => {
+                chai.expect(_adminUser).to.have.property('role');
+                chai.expect(_adminUser.role).to.equal('ADMIN_USER');
+              });
+              chai.expect(superAdminUserData).to.be.an('array');
+              chai.expect(superAdminUserData.length).to.be.equal(0);
+              done();
+            });
+        });
+    });
   });
 
   it('should give BASIC_USER data to a SUPER_ADMIN_USER', (done) => {
