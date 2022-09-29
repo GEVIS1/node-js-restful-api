54f03aa57329eb668ce10389128d2fb36f2a084c
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-09-30 12:03:11 +1300

    feat(routes): add getUser route

--- src/routes/v2/users.ts
+++ src/routes/v2/users.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getUsers, updateUser } from '../../controllers/v2/users';
+import { getUsers, getUser, updateUser } from '../../controllers/v2/users';
 
 router.route('/').get(getUsers);
+router.route('/:id').get(getUser);
 router.route('/:id').put(updateUser);
 
 export default router;
