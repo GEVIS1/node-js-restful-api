34b2bc9cf2180a94f22001abd5268eacab088a57
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-09-30 18:55:40 +1300

    refactor(routes/seed): update seed routes to use new createSeeder function

--- src/routes/v2/seed.ts
+++ src/routes/v2/seed.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 const router = Router();
 
-import { seedAdminUsers, seedUsers } from '../../controllers/v2/seed';
+import { createSeeder } from '../../controllers/v2/seed';
 
-router.route('/admin').post(seedAdminUsers);
+const { BASIC_USER_GIST, ADMIN_USER_GIST } = process.env;
+
+if (!process.env.BASIC_USER_GIST)
+  throw Error('BASIC_USER_GIST not set in .env');
+if (!process.env.ADMIN_USER_GIST)
+  throw Error('ADMIN_USER_GIST not set in .env');
+
+const seedUsers = createSeeder(BASIC_USER_GIST as string, 'BASIC_USER');
+const seedAdminUsers = createSeeder(ADMIN_USER_GIST as string, 'ADMIN_USER');
+
 router.route('/user').post(seedUsers);
+router.route('/admin').post(seedAdminUsers);
 
 export default router;
