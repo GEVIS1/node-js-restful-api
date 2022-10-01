ab95e081d79cf14d739377a0f406272cf2435e37
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-01 21:29:03 +1300

    feat(routes/auth): add logout route

--- src/routes/v2/auth.ts
+++ src/routes/v2/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
 
-import { register, login } from '../../controllers/v2/auth';
+import { register, login, logout } from '../../controllers/v2/auth';
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/logout').get(logout);
 
 export default router;
