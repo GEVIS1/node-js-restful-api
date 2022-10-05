612ead7888efefa89b4e77e8c8b3760ee34944df
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-05 19:20:22 +1300

    test: remove delay in before hook
    clearDataAndResetIds now takes long enough for the delay to be unnecessary

--- test/v2/src/00-setup.test.ts
+++ test/v2/src/00-setup.test.ts
@@ -1,24 +1,16 @@
-import { setTimeout } from 'timers/promises';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 
 import app from '../../../src/app';
 import { clearDataAndResetIds } from '../misc/resetIds';
 
-export const SECOND = 1000;
-const DELAY = 5 * SECOND;
-
 chai.use(chaiHttp);
 
 export const agent = chai.request.agent(app);
 export const closeAgent = () => agent.close();
 
-before(async function () {
+before('Resetting database', async function () {
   await clearDataAndResetIds();
   // eslint-disable-next-line no-console
   console.log();
-  /**
-   * Add delay after initializing the agent so that it is ready before we start testing
-   */
-  await setTimeout(DELAY);
 });
