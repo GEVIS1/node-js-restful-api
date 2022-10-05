8c41311d0946a5fcda5f71f8a04ad1bd529469a1
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-05 16:31:32 +1300

    feat(utils): add axios instances for the quiz API

--- src/utils/v2/axios.ts
+++ src/utils/v2/axios.ts
@@ -12,4 +12,45 @@ const getAvatar = axios.create({
   baseURL: avatarBaseUrl,
 });
 
-export { getAvatar, avatarBaseUrl };
+/**
+ * URL to the API used for quiz data
+ */
+const quizBaseUrl = 'https://opentdb.com/';
+
+/**
+ * URL to the quiz categories
+ */
+const quizCategoriesUrl = `${quizBaseUrl}api_category.php`;
+
+/**
+ * Default quiz parameters
+ */
+const defaultQuizParams = '?amount=10&encode=base64';
+
+/**
+ * URL to the quiz categories
+ */
+const quizDataUrl = `${quizBaseUrl}api.php${defaultQuizParams}`;
+
+/**
+ * An axios instance with the baseURL set to the quiz categories
+ */
+const getCategories = axios.create({
+  baseURL: quizCategoriesUrl,
+});
+
+/**
+ * An axios instance with the baseURL set to the quiz categories
+ */
+const getQuiz = axios.create({
+  baseURL: quizDataUrl,
+});
+
+export {
+  avatarBaseUrl,
+  getAvatar,
+  quizBaseUrl,
+  quizCategoriesUrl,
+  getCategories,
+  getQuiz,
+};
