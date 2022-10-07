e8fc0fff573a28eecc65ba8ba84496120b68b646
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-07 20:38:04 +1300

    test(04-quiz.test.ts): add tests for creating quizzes
    tests for both ADMIN_USER and SUPER_ADMIN_USER roles

--- test/v2/src/04-quiz.test.ts
+++ test/v2/src/04-quiz.test.ts
@@ -1,5 +1,8 @@
+import { Difficulty } from '@prisma/client';
 import chai from 'chai';
-import { user } from '../misc/userdata';
+import { yoda } from '../../../prisma/v2/seeder/users';
+import { getNewDateWithAddedDays } from '../../../src/controllers/v2/quizzes';
+import { getAdminUser, user } from '../misc/userdata';
 import { agent } from './00-setup.test';
 
 describe('It should create quizzes', () => {
@@ -17,5 +20,167 @@ describe('It should create quizzes', () => {
     chai.expect(postResponse.body).to.be.an('object');
     chai.expect(postResponse.body.success).to.be.equal(false);
     chai.expect(postResponse.body.error).to.equal('Unauthorized');
+  });
+
+  it('should let an ADMIN_USER create a quiz', async () => {
+    const loginResponse = await agent
+      .post('/api/v2/auth/login')
+      .send(await getAdminUser());
+
+    const quizName = 'Admin User User quiz';
+    const startDate = new Date();
+    const endDate = getNewDateWithAddedDays(startDate, 5);
+    const difficulties = [...Object.values(Difficulty)];
+    const difficulty =
+      difficulties[Math.floor(Math.random() * difficulties.length)];
+    const numberOfQuestions = 10;
+
+    const postResponse = await agent
+      .post(`/api/v2/quizzes`)
+      .set({ Authorization: `Bearer ${loginResponse.body.token}` })
+      .send({
+        name: quizName,
+        startDate,
+        endDate,
+        difficulty,
+        numberOfQuestions,
+      });
+
+    chai.expect(postResponse.status).to.be.equal(201);
+    chai.expect(postResponse.body).to.be.an('object');
+    chai.expect(postResponse.body.success).to.be.equal(true);
+    chai
+      .expect(postResponse.body.message)
+      .to.equal(`Successfully created quiz '${quizName}'`);
+    chai.expect(postResponse.body).to.have.property('data');
+    chai.expect(postResponse.body.data).to.be.an('object');
+    chai.expect(postResponse.body.data.name).to.equal(quizName);
+    chai.expect(postResponse.body.data).to.have.property('startDate');
+    chai.expect(postResponse.body.data.startDate).to.equal(startDate.toJSON());
+    chai.expect(postResponse.body.data).to.have.property('endDate');
+    chai.expect(postResponse.body.data.endDate).to.equal(endDate.toJSON());
+    chai.expect(postResponse.body.data).to.have.property('difficulty');
+    chai.expect(postResponse.body.data.difficulty).to.equal(difficulty);
+    chai.expect(postResponse.body.data).to.have.property('numberOfQuestions');
+    chai
+      .expect(postResponse.body.data.numberOfQuestions)
+      .to.equal(numberOfQuestions);
+    chai.expect(postResponse.body.data).to.have.property('questions');
+    chai.expect(postResponse.body.data.questions).to.be.an('array');
+    chai
+      .expect(postResponse.body.data.questions)
+      .to.be.of.length(numberOfQuestions);
+
+    chai.expect(postResponse.body.data.questions[0]).to.be.an('object');
+    chai.expect(postResponse.body.data.questions[0]).to.have.property('id');
+
+    // Fetch question so we can compare it
+    const question = await prisma?.question.findFirst({
+      where: {
+        id: postResponse.body.data.questions[0].id,
+      },
+    });
+
+    if (!question) throw Error('Could not find question.');
+
+    chai.expect(postResponse.body.data.questions[0].id).to.equal(question.id);
+    chai
+      .expect(postResponse.body.data.questions[0].categoryId)
+      .to.equal(question.categoryId);
+    chai
+      .expect(postResponse.body.data.questions[0].type)
+      .to.equal(question.type);
+    chai
+      .expect(postResponse.body.data.questions[0].difficulty)
+      .to.equal(question.difficulty);
+    chai
+      .expect(postResponse.body.data.questions[0].question)
+      .to.equal(question.question);
+    chai
+      .expect(postResponse.body.data.questions[0].correctAnswer)
+      .to.equal(question.correctAnswer);
+    chai
+      .expect(postResponse.body.data.questions[0].incorrectAnswers)
+      .to.deep.equal(question.incorrectAnswers);
+  });
+
+  it('should let a SUPER_ADMIN_USER create a quiz', async () => {
+    const loginResponse = await agent.post('/api/v2/auth/login').send(yoda);
+
+    const quizName = 'Super Admin User User quiz';
+    const startDate = new Date();
+    const endDate = getNewDateWithAddedDays(startDate, 5);
+    const difficulties = [...Object.values(Difficulty)];
+    const difficulty =
+      difficulties[Math.floor(Math.random() * difficulties.length)];
+    const numberOfQuestions = 10;
+
+    const postResponse = await agent
+      .post(`/api/v2/quizzes`)
+      .set({ Authorization: `Bearer ${loginResponse.body.token}` })
+      .send({
+        name: quizName,
+        startDate,
+        endDate,
+        difficulty,
+        numberOfQuestions,
+      });
+
+    chai.expect(postResponse.status).to.be.equal(201);
+    chai.expect(postResponse.body).to.be.an('object');
+    chai.expect(postResponse.body.success).to.be.equal(true);
+    chai
+      .expect(postResponse.body.message)
+      .to.equal(`Successfully created quiz '${quizName}'`);
+    chai.expect(postResponse.body).to.have.property('data');
+    chai.expect(postResponse.body.data).to.be.an('object');
+    chai.expect(postResponse.body.data.name).to.equal(quizName);
+    chai.expect(postResponse.body.data).to.have.property('startDate');
+    chai.expect(postResponse.body.data.startDate).to.equal(startDate.toJSON());
+    chai.expect(postResponse.body.data).to.have.property('endDate');
+    chai.expect(postResponse.body.data.endDate).to.equal(endDate.toJSON());
+    chai.expect(postResponse.body.data).to.have.property('difficulty');
+    chai.expect(postResponse.body.data.difficulty).to.equal(difficulty);
+    chai.expect(postResponse.body.data).to.have.property('numberOfQuestions');
+    chai
+      .expect(postResponse.body.data.numberOfQuestions)
+      .to.equal(numberOfQuestions);
+    chai.expect(postResponse.body.data).to.have.property('questions');
+    chai.expect(postResponse.body.data.questions).to.be.an('array');
+    chai
+      .expect(postResponse.body.data.questions)
+      .to.be.of.length(numberOfQuestions);
+
+    chai.expect(postResponse.body.data.questions[0]).to.be.an('object');
+    chai.expect(postResponse.body.data.questions[0]).to.have.property('id');
+
+    // Fetch question so we can compare it
+    const question = await prisma?.question.findFirst({
+      where: {
+        id: postResponse.body.data.questions[0].id,
+      },
+    });
+
+    if (!question) throw Error('Could not find question.');
+
+    chai.expect(postResponse.body.data.questions[0].id).to.equal(question.id);
+    chai
+      .expect(postResponse.body.data.questions[0].categoryId)
+      .to.equal(question.categoryId);
+    chai
+      .expect(postResponse.body.data.questions[0].type)
+      .to.equal(question.type);
+    chai
+      .expect(postResponse.body.data.questions[0].difficulty)
+      .to.equal(question.difficulty);
+    chai
+      .expect(postResponse.body.data.questions[0].question)
+      .to.equal(question.question);
+    chai
+      .expect(postResponse.body.data.questions[0].correctAnswer)
+      .to.equal(question.correctAnswer);
+    chai
+      .expect(postResponse.body.data.questions[0].incorrectAnswers)
+      .to.deep.equal(question.incorrectAnswers);
   });
 });
