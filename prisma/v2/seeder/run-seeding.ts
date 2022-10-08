bd7a96ce4aff3d374c132d7ae63b897f6768f756
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-08 17:06:22 +1300

    feat(prisma/seeder): add quiz seeding to seeding script

--- prisma/v2/seeder/run-seeding.ts
+++ prisma/v2/seeder/run-seeding.ts
@@ -1,8 +1,14 @@
-import { seedCategories, seedQuestions, seedSuperAdminUsers } from './seeders';
+import {
+  seedCategories,
+  seedQuestions,
+  seedQuizzes,
+  seedSuperAdminUsers,
+} from './seeders';
 try {
   await seedSuperAdminUsers();
   await seedCategories();
   await seedQuestions();
+  await seedQuizzes();
 } catch (err) {
   // eslint-disable-next-line no-console
   console.log(err);
