88a8416c0252fb302a3c98a01d5eb7d8fa9d8daa
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-09 17:06:29 +1300

    feat(routes/quiz): add deleteQuiz route

--- src/routes/v2/quizzes.ts
+++ src/routes/v2/quizzes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 const router = Router();
 
-import { createQuiz, getQuizzes } from '../../controllers/v2/quizzes';
+import {
+  createQuiz,
+  deleteQuiz,
+  getQuizzes,
+} from '../../controllers/v2/quizzes';
 
 router.route('/').get(getQuizzes);
 router.route('/').post(createQuiz);
+router.route('/:id').delete(deleteQuiz);
 
 export default router;
