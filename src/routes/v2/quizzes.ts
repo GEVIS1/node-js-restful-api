e961a1d6b3581f7e10abf7c4008f09b1f832d41a
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-12 21:23:13 +1300

    feat(routes/quizzes): add route to rateQuiz

--- src/routes/v2/quizzes.ts
+++ src/routes/v2/quizzes.ts
@@ -6,11 +6,13 @@ import {
   deleteQuiz,
   getQuizzes,
   participateInQuiz,
+  rateQuiz,
 } from '../../controllers/v2/quizzes';
 
 router.route('/').get(getQuizzes);
 router.route('/').post(createQuiz);
 router.route('/:id').delete(deleteQuiz);
 router.route('/:id').post(participateInQuiz);
+router.route('/rate/:id').post(rateQuiz);
 
 export default router;
