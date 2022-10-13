ebee87b1fd04c439ef7e68c587219d5087e8b5d2
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-10-13 14:49:37 +1300

    fix(controllers/scores): correctly type getScoresSelect

--- src/controllers/v2/scores.ts
+++ src/controllers/v2/scores.ts
@@ -1,10 +1,11 @@
+import { Prisma } from '@prisma/client';
 import { Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import { AuthorizedRequest } from '../../middleware/v2/authorization/authRoute';
 import { RequestError } from './misc/RequestError';
 
-export const getScoresSelect = {
+export const getScoresSelect: Prisma.ScoreSelect = {
   id: true,
   quizId: true,
   quiz: {
